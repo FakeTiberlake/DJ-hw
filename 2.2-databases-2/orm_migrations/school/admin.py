67c71c7a8e896fa4ada49c873b7c5186d134076a
Snezhana Yakovkina <snezhanayakovkina@gmail.com>
2022-09-22 14:28:12 +0900

    migrations added

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'group']
+    list_filter = ['teachers']
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'subject']
+    list_filter = ['subject']
