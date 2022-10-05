8706cf5e6476ecfd82e7fa2e364d302686aa2318
Snezhana Yakovkina <snezhanayakovkina@gmail.com>
2022-10-05 13:12:03 +0900

    smart_home: metaclass conflict

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,43 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework import generics
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, ListCreateAPIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer
+
+
+# создание датчика и получение списка с краткой информацией
+class SensorView(generics, ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+# изменение датчика и получение данных по конкретному датчику
+class SensorRetrieveUpdate(generics, RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+# добавляет измерение
+class MeasurementAPIView(generics, CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# изменение и получение инфы о датчике конкретном??
+# class SensorAPIView(generics.RetrieveUpdateAPIView):
+#     queryset = Sensor.objects.all()
+#     serializer_class = SensorSerializer
+
+
